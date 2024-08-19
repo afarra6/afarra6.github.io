2d9ba1d1af441d8cf11086fc79f83d94a8c93987
afarra6 <afarra6@lsu.edu>
2024-08-18 22:23:52 -0500

    added transitions and about page

--- components.js
+++ components.js
@@ -1,7 +1,7 @@
 class MyHeader extends HTMLElement {
     connectedCallback(){
         this.innerHTML =`
-    <p class="my_header">
+    <div class="header_flex">
         <a class="header_link" href="index.html">Home</a>
         <a class="header_link" href="about.html">About</a>
         <a class="header_link" href="works.html">Works</a>
